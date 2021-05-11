2c8cdaf7ccdebf41fc90531eba3cc70769fc5b97
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-05-11 06:55:02 +0530

    Update factor_finding.py

--- Code_chef/G_S_P_C/factor_finding.py
+++ Code_chef/G_S_P_C/factor_finding.py
@@ -1,7 +1,12 @@
 # cook your dish here
 num =  int(input())
 count = 0 
+l = []
 for i in range(1,num+1):
     if num%i == 0:
         count +=1
+        l.append(i)
 print(count)
+for e in l:
+    print(e,end=" ")
+print()
