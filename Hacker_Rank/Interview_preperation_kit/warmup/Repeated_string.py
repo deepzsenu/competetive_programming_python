1feff3c62f83e0828bbf75e1270c21214fa7d812
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-05-11 20:05:42 +0530

    Update Repeated_string.py

--- Hacker_Rank/Interview_preperation_kit/warmup/Repeated_string.py
+++ Hacker_Rank/Interview_preperation_kit/warmup/Repeated_string.py
@@ -6,24 +6,21 @@ import random
 import re
 import sys
 
-#
-# Complete the 'repeatedString' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts following parameters:
-#  1. STRING s
-#  2. LONG_INTEGER n
-#
-
+# Complete the repeatedString function below.
 def repeatedString(s, n):
-    # Write your code here
+    number = n//len(s)
+    reminder = n%len(s)
+    result = number*s.count("a")+ s[0:reminder].count("a")
+    
+    return result
+    
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     s = input()
 
-    n = int(input().strip())
+    n = int(input())
 
     result = repeatedString(s, n)
 
