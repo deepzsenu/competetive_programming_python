940b9e32dd0c4ff72c5d05601b77bf0787698f5d
Deepak Kumar Saxena <deepzsenu@gmail.com>
2021-07-17 20:40:30 +0530

    Update 2D_array_Ds.py

--- Hacker_Rank/Interview_preperation_kit/Arrays/2D_array_Ds.py
+++ Hacker_Rank/Interview_preperation_kit/Arrays/2D_array_Ds.py
@@ -1,1 +1,40 @@
 #2darray
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    n = float("-inf")
+    for i in range(len(arr)-2):
+        for j in range(len(arr)-2):
+            total = sum(arr[i][j:j+3])+ arr[i+1][j+1] + sum(arr[i+2][j:j+3])
+            
+            if total> n:
+                n = total
+    return n
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
